37fdfcb601739cb1f0a82e960f3bca10fb0f5dff
Julian McDonald <warcraft_666_2@hotmail.com>
2026-04-18 17:08:57 +1000

    fix(pybind): Restore get_specimens and get_research_bottom_col bindings

--- open_spiel/python/pybind11/games_dune_imperium.cc
+++ open_spiel/python/pybind11/games_dune_imperium.cc
@@ -300,6 +300,8 @@ void open_spiel::init_pyspiel_games_dune_imperium(py::module& m) {
     .def("get_player_agents_remaining",
          &DuneImperiumState::GetPlayerAgentsRemainingForTesting,
          py::arg("player"))
+    .def("get_specimens", &DuneImperiumState::GetSpecimensForTesting,
+         py::arg("player"))
     .def("get_player_cards_in_hand", &DuneImperiumState::GetPlayerCardsInHand,
          py::arg("player"))
     .def("get_pending_intrigue_player", &DuneImperiumState::pending_intrigue_draws_player)
@@ -348,6 +350,8 @@ void open_spiel::init_pyspiel_games_dune_imperium(py::module& m) {
     .def("is_space_reachable", &DuneImperiumState::IsSpaceReachableForTesting,
          py::arg("space_action"))
     .def("get_tleilaxu_track", &DuneImperiumState::GetTleilaxuTrackForTesting,
+         py::arg("player"))
+    .def("get_research_bottom_col", &DuneImperiumState::GetResearchBottomColForTesting,
          py::arg("player"))
     .def("get_research_bottom_row", &DuneImperiumState::GetResearchBottomRowForTesting,
          py::arg("player"))
